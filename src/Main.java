2aade90b59809661221f519b490cf8b302b5317e
Anatoly Manakov <anatolymanakov@gmail.com>
2022-06-18 11:38:04 +0300

    третий коммит - 3-я строка текста

--- src/Main.java
+++ src/Main.java
@@ -2,6 +2,7 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
         System.out.println("Пробуем сделать ДЗ WTF");
+        System.out.println("Теперь не понятно отправилось на Гит или нет");
     }
 
 }
